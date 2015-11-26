ebf6da4fef6484b1b4aab5d07b41a5673cfc9539
Jas Der <jasmineder@gmail.com>
2015-11-26 00:09:25 +0000

    first working version

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
+var resultsTemplate = _.template($('#results-template').html());
+
 $(function() {
 
-  var input = $('#search-term').val();
-  var request = {
-    url: 'http://api.giphy.com/v1/gifs/search',
-    data: { q: input, limit: 10, api_key: 'dc6zaTOxFJmzC'}
-  };
+  $('#gif-search').on('submit', function(event) {
+    event.preventDefault();
+    console.log("is this working?");
 
-  $.ajax(request).then(function(data) {
-    console.log(data);
-    var results = ''
+    var input = $('#search-term').val();
+    var request = {
+      url: 'http://api.giphy.com/v1/gifs/search',
+      data: { q: input, limit: 10, api_key: 'dc6zaTOxFJmzC'}
+    };
 
-    data.
-  });
+    $.ajax(request).then(function(data) {
+      console.log(data);
+      var output = ''
 
+      data.data.forEach(function(gif) {
+        output = output + resultsTemplate({
+          bitly_gif_url: gif.bitly_gif_url,
+          rating: gif.rating,
+          img_src: gif.images.fixed_height.url
+        });
+      });
 
+      $('#result').html(output, function() {
+        console.log("this was triggered")
+      });
+
+    }).fail(function(){
+      console.log("this failed");
+    });
+
+  })
 });
